5519ba6483cc18d22659a7a9290476043ee7975a
Edward Obert <56443800+EdwardObert@users.noreply.github.com>
2020-05-29 22:04:11 +0200

    Add files via upload

---
+++ Aufgabe_6/Test.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Http = require("http");
+var L06_Cocktailbar;
+(function (L06_Cocktailbar) {
+    let server = Http.createServer();
+    console.log(server);
+})(L06_Cocktailbar = exports.L06_Cocktailbar || (exports.L06_Cocktailbar = {}));
+//# sourceMappingURL=Test.js.map
